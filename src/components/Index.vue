6f21d83e28a8e04d45a4331ca6b3924dfa031932
kakiuchi yusuke <ka030709@gmail.com>
2019-12-11 15:51:40 +0900

    データ調整

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -3,8 +3,10 @@
     <h1>商品詳細画面</h1>
     <div id="products">
       <h2>{{ state.productName }}</h2>
+      <img :src="imageUrl" />
       <p>{{ state.productPrice }}円</p>
       <p>{{ state.productDescription }}</p>
+      <p>{{ state.productBrand }}</p>
     </div>
   </div>
 </template>
@@ -12,26 +14,34 @@
 const ProductInfo = [
   {
     productName: '四季彩の丘',
-    productImageUrl: 'test.png',
+    productImageUrl: 'product_image/product000.png',
     productDescription:
       '夏のグリーンシーズンは、色鮮やかな花畑や雄大な景色を。 冬のウィンターシーズンは、雪化粧した丘で迫力満点のアクティビティをお楽しみください。',
-    productPrice: 25000
+    productPrice: 25000,
+    productBrand: '北海道・美瑛の「四季彩の丘」'
   },
   {
     productName: '天に続く道',
-    productImageUrl: 'test.png',
+    productImageUrl: 'product_image/product001.png',
     productDescription:
       '天に続く道は、北海道斜里町にある全長約28.1キロメートルの直線道路です。真っ直ぐな道がはるか遠くまで続き、あたかも天まで続いているように見えることから名付けられました。おすすめの時期は、春分の日と秋分の日辺りの年に2回で、道の延長上に夕日が沈んでいく様子を見ることができます。また、付近には名もなき展望台があり、オホーツク海や田畑を見渡すことができます。',
-    productPrice: 19000
+    productPrice: 19000,
+    productBrand: '北海道の国道334・244号線'
   }
 ]
 
 export default{
   data () {
-    const id = this.$route.query.id
+    let id = this.$route.query.id
+    if (!id) {
+      id = 0
+    }
     const state = ProductInfo[id]
+    const image = state.productImageUrl
     return {
-      state
+      state,
+      imageUrl: require(`../assets/${image}`),
+      id
     }
   }
 }
@@ -46,6 +56,8 @@ export default{
   margin-top: 60px;
 }
 img {
+  width: 200px;
+  height: 200px;
   border: 1px black solid;
 }
 </style>
